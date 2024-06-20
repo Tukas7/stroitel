abd59280f57ec62c2739968a5d81d33eab143078
Tukas7 <121289359+Tukas7@users.noreply.github.com>
2024-06-20 11:04:43 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'monorail.proxy.rlwy.net',
-    database: 'railway',
-    password: 'iVLJcjIHCfPxyWARweRYhRPEqKqEtQGT',
-    port: 32498,
+    host: 'viaduct.proxy.rlwy.net',
+    database: 'stroi',
+    password: 'VABbgCjkjLffcsYvmgjOYcWeUmolovRb',
+    port: 13266,
 });
 
 module.exports = pool;
